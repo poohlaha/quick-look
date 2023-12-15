526ff1ca26d3b728de12d442f152ec2eab556f07
poohlaha <>
2023-12-15 09:36:27 +0800

    修改图标,添加系统托盘

--- packages/enclosure/src-tauri/src/main.rs
+++ packages/enclosure/src-tauri/src/main.rs
@@ -8,6 +8,8 @@ mod preview;
 mod process;
 mod utils;
 
+use tauri::Manager;
+use tauri::tray::{ClickType, TrayIconBuilder};
 use analysis::{file_handler, read_file_association, unarchive};
 
 fn main() {
@@ -16,6 +18,21 @@ fn main() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_log::Builder::default().build())
+        .setup(move |app| {
+            let _ = TrayIconBuilder::with_id("quick-look-tray")
+                .icon(app.default_window_icon().unwrap().clone())
+                .icon_as_template(true)
+                .on_tray_icon_event(|tray, event| {
+                    if event.click_type == ClickType::Left {
+                        let app = tray.app_handle();
+                        if let Some(window) = app.get_window("main") {
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    }
+                }).build(app);
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![file_handler, unarchive])
         .run(tauri::generate_context!())
         .expect("error while running QuickLook application");
